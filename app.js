1dbf4a5b10b939d5c0c29ad539fa32f51c2581be
JeremyVe <jepschx@gmail.com>
2016-08-31 16:32:11 +0800

    Child state routing done

--- app.js
+++ app.js
@@ -14,10 +14,30 @@ TargetPractice.config(
 						template: 'Targeting unnamed view from parent state'
 					},
 					"named-parent": {
-						template: 'Target named view from parent state'
+						template: 'Target named parent view from parent state'
 					},
 					'main-header': {
 						template: 'Targeting header from parent state'
+					}
+				}
+			})
+			.state('parent.child', {
+				url: 'child',
+				views: {
+					'@': {
+						templateUrl: 'templates/child.html'
+					},
+					'@parent.child': {
+						template: 'Targeting unnamed view child from child state'
+					},
+					'name-child@parent.child': {
+						template: 'Targeting named child from child state'
+					},
+					'named-parent@': {
+						template: 'Targeting named parent view from child state'
+					},
+					'main-header@': {
+						template: 'Targeting main header from child state'
 					}
 				}
 			})
