aefe869402d1b457e5ccb75fa198dfd7286c2f8d
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-29 09:36:00 -0700

    Finish all templates

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ TargetPractice.config(['$stateProvider', '$urlRouterProvider', function($statePr
 
   $stateProvider
     .state('parent', {
-      url: '/',
+      url: '',
       views: {
         '': {
           template: 'Targeting unnamed view from parent state'
@@ -22,5 +22,59 @@ TargetPractice.config(['$stateProvider', '$urlRouterProvider', function($statePr
 
       }
     })
+
+    .state('parent.child', {
+      url: '/child',
+      views: {
+        '@': {
+          templateUrl: 'templates/child.html'
+        },
+
+        '@parent.child': {
+          template: 'Targeting unnamed child view from child state'
+        },
+
+        'named-child@parent.child': {
+          template: 'Targeting named child view from child state'
+        },
+
+        'named-parent@' : {
+          template: 'Targeting named parent view from child state'
+        },
+
+        // 'main-header@': {
+        //   template: 'Targeting header from child state'
+        // }
+      }
+    })
+
+    .state('parent.child.grandchild', {
+      url: '/grandchild',
+      views: {
+        '@parent.child': {
+          templateUrl: "templates/grandchild.html"
+        },
+
+        '@parent.child.grandchild': {
+          template: 'Targeting unnamed grandchild view from grandchild state'
+        },
+
+        'named-grandchild@parent.child.grandchild': {
+          template: 'Targeting named grandchild view from grandchild state'
+        },
+
+        'named-child@parent.child': {
+          template: 'Targeting named child view from grandchild state'
+        },
+
+        'named-parent@': {
+          template: 'Targeting named parent view from grandchild state'
+        },
+
+        'main-header@': {
+          template: 'Targeting header from grandchild state'
+        }
+      }
+    });
 
 }]);
