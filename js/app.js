f25898d5c74888e1f9ff2670c9c38f176b427f28
cjvirtucio87 <cjv287@gmail.com>
2016-09-29 09:16:37 -0700

    parent

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('ourApp', []);
+var app = angular.module('ourApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('parent', {
+      url: '/',
+      views: {
+        '': {
+          template: 'Targeting an unnamed view from the parent.'
+        },
+        'named-parent': {
+          template: 'Targeting named view from parent state.'
+        },
+        'main-header': {
+          template: 'Targeting header from parent state.'
+        }
+      }
+    });
+
+});
