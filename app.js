e1f3e9bdfb37633382c1707beab768ea9850cf85
Morgan Martin <morganm5201@gmail.com>
2016-09-29 12:41:26 -0400

    PULL!

--- app.js
+++ app.js
@@ -21,7 +21,28 @@ targetPractice.config(function($stateProvider, $urlRouterProvider){
           template: 'Targeting header from parent state'
         }
       }
-    });
+    })
+    .state('parent.child', {
+      url: 'child',
+      views: {
+        '@': {
+          templateUrl: 'templates/child.html'
+        },
+        '@parent.child': {
+          template: 'Targeting unnamed child view from child state'
+        },
+        'named-child@parent.child': {
+          template: 'Targeting named-child view from child state'
+        },
+        'named-parent@': {
+          template: 'Targeting named-parent view from child state'
+        },
+        'main-header@': {
+          template: 'Targeting main-header from child state'
+        }
+      }
+    })
+  ;
 });
 
 // ADDING ERROR HANDLING
