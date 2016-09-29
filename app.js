02486dde6d3f065aca7569c9fce91764f871082d
Morgan Martin <morganm5201@gmail.com>
2016-09-29 12:31:56 -0400

    views showing

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-var TargetPractice = angular.module('TargetPractice', []);
+var targetPractice = angular.module('targetPractice', ['ui.router']);
+
+targetPractice.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("");
+
+  $stateProvider
+    .state('parent', {
+      url: '/',
+      views: {
+
+        '': {
+          template: 'Targeting unnamed view from parent state'
+        },
+
+        'named-parent': {
+          template: 'Targeting named view from parent state'
+        },
+
+        'main-header': {
+          template: 'Targeting header from parent state'
+        }
+      }
+    });
+});
+
+// ADDING ERROR HANDLING
+targetPractice.run(function($rootScope){
+  $rootScope.$on("$stateChangeError", console.log.bind(console));
+});
