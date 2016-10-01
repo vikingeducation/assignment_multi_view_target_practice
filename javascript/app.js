6f9584c5c4c145b91e0a46eaa3b2dbae0644c168
BranLiang <lby89757@hotmail.com>
2016-10-01 20:34:09 +0800

    finish

--- javascript/app.js
+++ javascript/app.js
@@ -33,4 +33,27 @@ TargetPractice.config(function ($stateProvider, $urlRouterProvider) {
 				}
 			}
 		})
+		.state('parent.child.grandchild', {
+			url: '/grandchild',
+			views: {
+				'@parent.child': {
+					templateUrl: 'javascript/templates/grandchild.html',
+				},
+				'@parent.child.grandchild': {
+					template: 'Targeting unnamed grandchild view from grandchild state',
+				},
+				'named-grandchild@parent.child.grandchild': {
+					template: 'Targeting named grandchild view from grandchild state',
+				},
+				'named-child@parent.child': {
+					template: 'Targeting named child view from grandchild state',
+				},
+				'named-parent@': {
+					template: 'Targeting named parent view from grandchild state',
+				},
+				'main-header@': {
+					template: 'Targeting header from grandchild state',
+				}
+			}
+		})
 });
