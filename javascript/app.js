0f685c7c5c5269d8086be43c111a6830d37924e0
BranLiang <lby89757@hotmail.com>
2016-10-01 20:15:37 +0800

    finish the baisc

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,19 @@
-var TargetPractice = angular.module('TargetPractice', []);
+var TargetPractice = angular.module('TargetPractice', ['ui.router']);
+
+TargetPractice.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('parent', {
+			url: '/',
+			views: {
+				'@': {
+					template: "Targeting unnamed view from parent state",
+				},
+				'named-parent@': {
+					template: "Targeting named view from parent state",
+				},
+				'main-header@': {
+					template: "Targeting header from parent state",
+				}
+			}
+		})
+});
