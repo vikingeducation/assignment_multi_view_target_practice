e8f0753e2b44587d89f37290cae09acd50c85ffb
schoetlr <srlrs20020@gmail.com>
2016-10-02 15:22:39 -0400

    display grandchild state

--- javascripts/app.js
+++ javascripts/app.js
@@ -52,6 +52,35 @@ TargetPractice.config(function($stateProvider, $urlRouterProvider){
       }
     })
 
+    .state("parent.child.grandchild", {
+      url: "/grandchild",
+      views: {
+        "@parent.child": {
+          templateUrl: "javascripts/templates/grandchild.html"
+        },
+
+        "@parent.child.grandchild": {
+          template: "<p>Targeting unnamed grandchild view from grandchild state<p/>"
+        },
+
+        "named-grandchild@parent.child.grandchild": {
+          template: "<p>Targeting named grandchild view from grandchild state<p/>"
+        },
+
+        "named-child@parent.child": {
+          template: "<p>Targeting named child view from grandchild state</p>"
+        },
+
+        "named-parent@": {
+          template: "<p>Targeting named parent view from grandchild state</p>"
+        },
+
+        "main-header@": {
+          template: "<p>Targeting header from grandchild state</p>"
+        }
+      }
+    })
+
     
 
 });
