3e9b090672bd99b8b70da628474ec5f7a7d18d60
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-27 09:25:21 -0700

    target practice

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 var targetPractice = angular.module('targetPractice', ['ui.router'])
+
+targetPractice.config([
+  '$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('parent', {
+                    url: '/',
+                    views:{
+                      "":{
+                        template: "targeting unnamed view from parent"
+                      },
+                      "namedParent": {
+                        template: "targeting named view from parent"
+                      },
+                      "mainHeader": {
+                        template: "<code>Parent</code> state"
+                      }
+                    }
+                  })
+                  .state('parent.child', {
+                          url: 'child',
+                          views:{
+                            '@': {
+                              templateUrl: 'js/views/child.html'
+                            },
+                            "@parent.child":{
+                              template: "targeting unnamed view from child"
+                            },
+                            "namedChild@parent.child": {
+                              template: "targeting named view from child"
+                            },
+                            "namedParent@":{
+                              template: "targeting named parent view from child"
+                            },
+                            "mainHeader@": {
+                              template: "<code>Child</code> state"
+                            }
+                          }
+                        })
+  }
+])
