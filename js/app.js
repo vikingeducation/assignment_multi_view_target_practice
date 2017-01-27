2d434e5a22ad5bd4aa17200569068735ce59475f
Christian Carey <christianrcarey@gmail.com>
2017-01-27 11:37:56 -0500

    donezo

--- js/app.js
+++ js/app.js
@@ -19,25 +19,50 @@ targetPractice.config([
                       }
                     }
                   })
-                  .state('parent.child', {
-                          url: 'child',
-                          views:{
-                            '@': {
-                              templateUrl: 'js/views/child.html'
-                            },
-                            "@parent.child":{
-                              template: "targeting unnamed view from child"
-                            },
-                            "namedChild@parent.child": {
-                              template: "targeting named view from child"
-                            },
-                            "namedParent@":{
-                              template: "targeting named parent view from child"
-                            },
-                            "mainHeader@": {
-                              template: "<code>Child</code> state"
-                            }
-                          }
-                        })
+                  .state('child', {
+                    parent: 'parent',
+                    url: 'child',
+                    views:{
+                      '@': {
+                        templateUrl: 'js/views/child.html'
+                      },
+                      "@child":{
+                        template: "targeting unnamed view from child"
+                      },
+                      "namedChild@child": {
+                        template: "targeting named view from child"
+                      },
+                      "namedParent@":{
+                        template: "targeting named parent view from child"
+                      },
+                      "mainHeader@": {
+                        template: "<code>Child</code> state"
+                      }
+                    }
+                  })
+                  .state('grandchild', {
+                      parent: 'child',
+                      url: '/grandchild',
+                      views: {
+                        '@child': {
+                          templateUrl: 'js/views/grandchild.html'
+                        },
+                        'namedGrandchild@grandchild': {
+                          template: 'targeting named view from grandchild state'
+                        },
+                        '@grandchild': {
+                          template: 'targeting unnamed view from grandchild state'
+                        },
+                        'mainHeader@': {
+                          template: '<code>Grandchild</code> state'
+                        },
+                        "namedParent@": {
+                          template: 'targeting named parent view from grandchild state'
+                        },
+                        "namedChild@child": {
+                          template: "targeting named child view from grandchild state"
+                        }
+                      }
+                  })
   }
 ])
