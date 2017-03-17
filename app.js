3035645f62d89c5bcc4eebe8082ff32448a2c0e8
tt <ttwang.303@gmail.com>
2017-03-17 14:25:08 -0400

    child state

--- app.js
+++ app.js
@@ -18,6 +18,27 @@ TargetPractice.config(function($stateProvider, $urlRouterProvider){
         }
       }
     })
+
+    .state('parent.child', {
+      url: 'child',
+      views: {
+        "@": { //???
+          templateUrl: "js/templates/child.html"
+        },
+        "@parent.child": {
+          template: "Targeting unnamed child view from child state"
+        },
+        "named-child@parent.child": {
+          template: "Targeting named child view from child state"
+        },
+        "named-parent@": {
+          template: "Targeting named parent view from child state"
+        },
+        "main-header@": {
+          template: "Targeting header from child state"
+        }
+      }
+    })
 })
 
 TargetPractice.controller('MyCtrl',
