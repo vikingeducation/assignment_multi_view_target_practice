017b5c571195e6d77723380cbda3c9ccc59d3fe6
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-24 00:30:10 -0700

    begin 'child state'

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,9 +5,22 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("");
 
   $stateProvider
-    .state('products', {
-      url: "/",
+    .state('parent', {
+      url: '/',
+      views: {
+        "": {
+          template: 'Targeting unnamed view from parent state'
+        },
+        "named-parent": {
+          template: 'Targeting named view from parent state'
+        },
+        "main-header": {
+          template: 'Targeting header from parent state'
+        }
+      }
     })
+
+
 
 
 });
