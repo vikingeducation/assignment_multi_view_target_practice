defe94f9eee1cc0b5aeb6486b55135454555b65e
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-24 01:19:37 -0700

    complete

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,7 +6,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
     .state('parent', {
-      url: '/',
+      // url: '/parent',
+      url: '',
       views: {
         "": {
           template: 'Targeting unnamed view from parent state'
@@ -21,13 +22,13 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
 
     .state('parent.child', {
-      url: "child",
+      url: "/child",
       views: {
           "@": {
             templateUrl: "javascripts/templates/child.html"
           },
           "@parent.child": {
-            template: 'Targeting unnamed child view from child state!'
+            template: 'Targeting unnamed child view from child state!ZZ'
           },
           "named-child@parent.child": {
             template: 'Targeting named child view from child state~'
@@ -39,6 +40,35 @@ app.config(function($stateProvider, $urlRouterProvider) {
             template: 'Targeting header from child state--'
           }
       }
+    })
+
+    .state('parent.child.grandchild', {
+      url: "/grandchild",
+      views: {
+          "@parent.child": {
+            templateUrl: "javascripts/templates/grandchild.html"
+          },
+          "@parent.child.grandchild": {
+            template: 'Targeting unnamed grandchild view from grandchild state%%'
+          },
+
+          "named-grandchild@parent.child.grandchild": {
+            template: 'Targeting named grandchild view from grandchild state named-grandchild@2222'
+          },
+
+          "named-child@parent.child": {
+            template: 'Targeting named child view from grandchild state^^'
+          },
+
+          "named-parent@": {
+            template: 'Targeting named parent view from grandchild state---==--'
+          },
+
+          "main-header@": {
+            template: 'Targeting header from grandchild state!!1`'
+          }
+      }
+
     })
 
 
